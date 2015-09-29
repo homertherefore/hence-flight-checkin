1662882445a5a4eb7c406e9c07ab866f90cbd03c
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-09-29 10:55:57 -0400

    - getting effective usage of html binding to be leveraged in cards and it's extended components with ease

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,10 @@ gulp build
 cd ../hence-flight-checkin/bower_components/hence-card
 cp -vR ../../../hence-card/dist .
 cd ../..
+
+cd ../hence-html
+gulp build
+cd ../hence-flight-checkin/bower_components/hence-html
+cp -vR ../../../hence-html/dist .
+cd ../..
 gulp
